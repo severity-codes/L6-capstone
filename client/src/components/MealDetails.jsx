abd189177c2ebb6a58850c851b8d567981ccf370
Severity Codes <122500980+severity-codes@users.noreply.github.com>
2024-05-05 01:42:49 -0400

    you tube and ingredients working

--- client/src/components/MealDetails.jsx
+++ client/src/components/MealDetails.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
@@ -9,20 +10,34 @@ const MealDetails = () => {
 
   useEffect(() => {
     const fetchMealDetails = async () => {
+      setLoading(true);
       try {
-        const response = await axios.get(
-          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
-        );
+        const response = await axios.get(`/api/meals/details/${mealId}`);
         const mealData = response.data.meals[0];
         setMealDetails(mealData);
-        setLoading(false);
       } catch (error) {
         console.error("Error fetching meal details:", error);
+      } finally {
         setLoading(false);
       }
     };
-    fetchMealDetails();
+
+    if (mealId) {
+      fetchMealDetails();
+    }
   }, [mealId]);
+
+  const getIngredients = () => {
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = mealDetails[`strIngredient${i}`];
+      const measure = mealDetails[`strMeasure${i}`];
+      if (ingredient && ingredient !== "") {
+        ingredients.push(`${measure} ${ingredient}`);
+      }
+    }
+    return ingredients;
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -32,11 +47,39 @@ const MealDetails = () => {
     return <div>No meal details found</div>;
   }
 
+  const ingredientsList = getIngredients();
+
   return (
     <div>
       <h2>{mealDetails.strMeal}</h2>
       <img src={mealDetails.strMealThumb} alt={mealDetails.strMeal} />
+      <h3>Ingredients:</h3>
+      <ul>
+        {ingredientsList.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <h3>Instructions:</h3>
       <p>{mealDetails.strInstructions}</p>
+
+      <h4>Category</h4>
+
+      <p>{mealDetails.strArea}</p>
+      <h3>You Tube Video:</h3>
+      {mealDetails.strYoutube && (
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${mealDetails.strYoutube.split("=")[1]}`}
+          title={mealDetails.strMeal}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      )}
+
+
+
     </div>
   );
 };
