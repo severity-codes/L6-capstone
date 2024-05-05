7aa5974dfd7c192a211c731322e5a6b52c42cbde
Severity Codes <122500980+severity-codes@users.noreply.github.com>
2024-05-05 00:36:29 -0400

    this works to display meal details when mela is clicked

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React, { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Auth from "./components/Auth";
 import Profile from "./components/Profile";
-
+import MealDetails from "./components/MealDetails";
 import { UserContext } from "./context/UserProvider";
 import Home from "./components/Home";
 import "./App.css";
@@ -16,11 +16,12 @@ export default function App() {
     <div className="app">
       {localStorage.getItem("token") && <Navbar logout={logout} />}
       <Routes>
-        {localStorage.getItem("token")? (
+        {localStorage.getItem("token") ? (
           <>
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
-          
+            jsx
+            <Route path="/meal/:mealId" element={<MealDetails />} />
           </>
         ) : (
           <Route path="*" element={<Auth />} />
--- routes/mealsRouter.js
+++ routes/mealsRouter.js
@@ -4,7 +4,28 @@ require("dotenv").config();
 
 const mealsRouter = express.Router();
 const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY;
+mealsRouter.get("/details/:mealId", async (req, res) => {
+  const { mealId } = req.params;
+  const options = {
+    method: "GET",
+    url: "https://themealdb.p.rapidapi.com/lookup.php",
+    params: { i: mealId },
+    headers: {
+      "X-RapidAPI-Key": RAPIDAPI_KEY,
+      "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+    },
+  };
 
+  try {
+    const response = await axios.request(options);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching meal details:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Route to search for meals
 mealsRouter.get("/search", async (req, res) => {
   const { f: query } = req.query; // Extract the search query from the request query parameters
   try {
